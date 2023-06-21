e2cb4a79b0023febdef2af0adaca81c9a258c150
BJ Kwon <bkwon@qualtrics.com>
2023-06-21 14:48:00 -0400

    ADD str2num

--- builtin_functions.h
+++ builtin_functions.h
@@ -142,6 +142,7 @@ DECL_GATE(_tseqget)
 DECL_GATE(_tseqset)
 DECL_GATE(_error_warning)
 DECL_GATE(_test)
+DECL_GATE(_str2num)
 
 void _fm(skope* past, const AstNode* pnode, const vector<CVar>& args);
 void _str2num(skope* past, const AstNode* pnode, const vector<CVar>& args);
